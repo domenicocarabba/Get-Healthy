480984ca39cec93ba1de5fb0649fa9dfba224e0c
Domenico Carabba Tettamanti Radziwill <domenico@AirdiDomenico2.homenet.telecomitalia.it>
2025-10-28 22:27:59 +0100

    Remove duplicate data/partners.ts to fix build conflict

--- data/partners.js
+++ data/partners.js
@@ -5,8 +5,7 @@ export const partners = [
         name: "Deliveroo",
         url: "https://deliveroo.it/?affid=GETHEALTHY_DEMO",
         logo: "/partners/deliveroo.svg",
-        description:
-            "Ordina piatti sani dai ristoranti vicino a te, in pochi minuti.",
+        description: "Ordina piatti sani dai ristoranti vicino a te, in pochi minuti.",
         tags: ["delivery", "ristoranti"],
     },
     {
@@ -14,8 +13,7 @@ export const partners = [
         name: "Glovo",
         url: "https://glovoapp.com/?affid=GETHEALTHY_DEMO",
         logo: "/partners/glovo.svg",
-        description:
-            "Spesa e cibo a domicilio: ingredienti freschi per le tue ricette.",
+        description: "Spesa e cibo a domicilio: ingredienti freschi per le tue ricette.",
         tags: ["spesa", "ingredienti", "delivery"],
     },
     {
@@ -23,8 +21,7 @@ export const partners = [
         name: "Just Eat",
         url: "https://www.justeat.it/?affid=GETHEALTHY_DEMO",
         logo: "/partners/justeat.svg",
-        description:
-            "Tanti locali qualità con opzioni salutari e veg.",
+        description: "Tanti locali qualità con opzioni salutari e veg.",
         tags: ["delivery", "ristoranti"],
     },
 ];
